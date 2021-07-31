3712215ad0e1df68e3a19aa8f09a48ea28138aa9
Wayuni <smattsbiometric@gmail.com>
2021-07-31 14:46:35 +0800

    CreateApi

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+class ApiController extends Controller
+{
+    public function create(Request $request)
+    {
+        $students = new Student();
+
+        $students->fname = $request->input('fname');
+        $students->lname = $request->input('lname');
+        $students->email = $request->input('email');
+        $students->password = $request->input('password');
+
+        $students->save();
+        return response()->json($students);
+    }
+}
